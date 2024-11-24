03ab89ede5df5cff2d04d41ec985bebede6c8787
MVolynyak <volynyak_maksim@mail.ru>
2024-11-24 23:54:55 +0300

    Внесены правки в verstka.html

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/main.css';
+
+const App: React.FC = () => {
+    return (
+        <div className="app">
+        </div>
+    );
+};
+export default App;
